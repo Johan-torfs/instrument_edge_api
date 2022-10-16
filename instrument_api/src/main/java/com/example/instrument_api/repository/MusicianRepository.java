6b50ac60bca9a511dde8efa0ed70bc21a6b95a1d
Arslan Bulent <info.hecasolutions@gmail.com>
2022-10-16 22:24:51 +0200

    Queay MusicianRepository update

--- instrument_api/src/main/java/com/example/instrument_api/repository/MusicianRepository.java
+++ instrument_api/src/main/java/com/example/instrument_api/repository/MusicianRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface MusicianRepository extends JpaRepository<Musician, Integer> {
-    @Query("SELECT u FROM Musician u WHERE u.yearOfBirth BETWEEN (:year) and 3000")
+    @Query("SELECT u FROM Musician u where :year between u.yearOfBirth and u.yearOfDeath")
     List<Musician> findAllByFromYear(@Param("year") int year);
     List<Musician> findMusicianByInstrument_Id(int id);
     List<Musician> findMusicianByInstrument_Name(String name);
