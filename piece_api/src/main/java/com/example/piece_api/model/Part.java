20ec4d2f83a1c2c172c7354bfb03ec19ffb5de04
Johan Torfs <r0831282@student.thomasmore.be>
2022-10-17 19:26:57 +0200

    Weird bug

--- piece_api/src/main/java/com/example/piece_api/model/Part.java
+++ piece_api/src/main/java/com/example/piece_api/model/Part.java
@@ -4,6 +4,11 @@ public class Part {
     private int instrumentId;
     private String name;
 
+    public Part() {
+        this.instrumentId = 5;
+        this.name = "Solo";
+    }
+
     public Part(int instrumentId, String name) {
         this.instrumentId = instrumentId;
         this.name = name;
