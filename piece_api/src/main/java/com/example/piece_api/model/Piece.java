0a85948c8de513a530b46b24ef75afcdeed8181a
JohanTorfs <r0831282@student.thomasmore.be>
2022-10-28 12:18:24 +0200

    forgot to remove ArrayList

--- piece_api/src/main/java/com/example/piece_api/model/Piece.java
+++ piece_api/src/main/java/com/example/piece_api/model/Piece.java
@@ -3,7 +3,6 @@ package com.example.piece_api.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection = "pieces")
