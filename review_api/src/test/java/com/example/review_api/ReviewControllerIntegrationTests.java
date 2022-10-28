77446a522ed7ef1caf5c28983107251368c2a411
Ebert <ebert@r-vo.net>
2022-10-28 21:12:21 +0200

    Integration tests

--- review_api/src/test/java/com/example/review_api/ReviewControllerIntegrationTests.java
+++ review_api/src/test/java/com/example/review_api/ReviewControllerIntegrationTests.java
@@ -4,33 +4,33 @@ import com.example.review_api.model.Review;
 import com.example.review_api.repository.ReviewRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.GreaterThan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.*;
 
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class ReviewControllerUnitTests {
+class ReviewControllerIntegrationTests {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
-    @MockBean
+    @Autowired
     private ReviewRepository reviewRepository;
     private List<Review> reviewList = new ArrayList<Review>();
     private ObjectMapper mapper = new ObjectMapper();
@@ -39,7 +39,8 @@ class ReviewControllerUnitTests {
     @BeforeEach
     public void setup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
-        reviewList = new ArrayList<Review>();
+        //NOTE: deletes everything in the review database
+        reviewRepository.deleteAll();
 
         Review review1 = new Review("Requerdos de la Alhambra", 4, "Not a fan");
         review1.setId("634d92404b48d018675459r4");
@@ -54,15 +55,21 @@ class ReviewControllerUnitTests {
         reviewList.add(review2);
         reviewList.add(review3);
         reviewList.add(review4);
-        for (Review review : reviewList) {
-            System.out.println(review.getPieceName() + " (" + review.getRating() + "): " + review.getComment() );
-        }
+
+        reviewRepository.save(review1);
+        reviewRepository.save(review2);
+        reviewRepository.save(review3);
+        reviewRepository.save(review4);
+    }
+
+    @AfterEach
+    public void afterAllTests() {
+        //Also deletes other test data
+        reviewRepository.deleteAll();
     }
 
     @Test
     void onCall_getReviews_returnAllReviews() throws Exception {
-        given(reviewRepository.findAll()).willReturn(reviewList);
-
         mockMvc.perform(get("/"))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -83,8 +90,6 @@ class ReviewControllerUnitTests {
 
     @Test
     void onCallGivenIdOfReviewWithComment_getReviewById_returnReview() throws Exception {
-        given(reviewRepository.findReviewById(reviewList.get(0).getId())).willReturn(reviewList.get(0));
-
         mockMvc.perform(get("/{id}", reviewList.get(0).getId()))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -95,8 +100,6 @@ class ReviewControllerUnitTests {
 
     @Test
     void onCallGivenIdOfReviewWithoutComment_getReviewById_returnReview() throws Exception {
-        given(reviewRepository.findReviewById(reviewList.get(2).getId())).willReturn(reviewList.get(2));
-
         mockMvc.perform(get("/{id}", reviewList.get(2).getId()))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -107,10 +110,9 @@ class ReviewControllerUnitTests {
 
     @Test
     void onCallGivenNameOfPiece_getReviewsByPieceName_returnReviews() throws Exception {
-        given(reviewRepository.findReviewsByPieceName(reviewList.get(2).getPieceName())).willReturn(reviewList.subList(2, 4));
         mockMvc.perform(get("/piece/{name}", reviewList.get(2).getPieceName()))
-                .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0].pieceName",is("Asturias")))
                 .andExpect(jsonPath("$[0].rating",is(10)))
@@ -119,47 +121,45 @@ class ReviewControllerUnitTests {
                 .andExpect(jsonPath("$[1].rating",is(6)))
                 .andExpect(jsonPath("$[1].comment",is("It's okay")));
     }
-    //For the next 3 tests, something is wrong in how the repository saves data, this returns null
+
     @Test
     void whenPostReview_thenReturnJsonReview() throws Exception {
-        given(reviewRepository.findAll()).willReturn(reviewList);
-        Review newReview = new Review("Asturias", 2, ":(");
-        mockMvc.perform(post("/")
-                    .content(mapper.writeValueAsString(newReview))
+        Review review = new Review("Asturias", 2, "Test comment");
+        mockMvc.perform(post("/").content(mapper.writeValueAsString(review))
                     .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(MockMvcResultHandlers.print())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.pieceName",is("Asturias")))
-                .andExpect(jsonPath("$.rating",is(2)))
-                .andExpect(jsonPath("$.comment",is(":(")));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", notNullValue()))
+                .andExpect(jsonPath("$.pieceName", is("Asturias")))
+                .andExpect(jsonPath("$.rating", is(2)))
+                .andExpect(jsonPath("$.comment", is("Test comment")));
     }
 
     @Test
-    void givenReview_whenPutReview_thenReturnJsonReview() throws Exception {
-        Review newReview = new Review("Asturias", 2, ":(");
-        newReview.setId("634d92404b48d018675459r8");
-        given(reviewRepository.findReviewById("634d92404b48d018675459r8")).willReturn(newReview);
-        Review updatedReview = new Review("Asturias", 5, "Maybe I was a bit drastic");
-        mockMvc.perform(put("/{id}", newReview.getId())
-                    .content(mapper.writeValueAsString(updatedReview))
+    void whenPutReview_thenReturnJsonReview() throws Exception {
+        Review review = new Review("", 9, "A fan");
+
+        mockMvc.perform(put("/{id}", reviewList.get(0).getId())
+                    .content(mapper.writeValueAsString(review))
                     .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(MockMvcResultHandlers.print())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.pieceName",is("Asturias")))
-                .andExpect(jsonPath("$.rating",is(5)))
-                .andExpect(jsonPath("$.comment",is("Maybe I was a bit drastic")));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.pieceName", is("Requerdos de la Alhambra")))
+                .andExpect(jsonPath("$.rating", is(9)))
+                .andExpect(jsonPath("$.comment", is("A fan")));
     }
 
     @Test
     void givenReview_whenDeleteReview_thenStatusOk() throws Exception {
-        Review review = new Review("Asturias", 2, "A comment");
-        review.setId("E3");
-        given(reviewRepository.findReviewById(review.getId())).willReturn(review);
-
-        mockMvc.perform(delete("/{id}", review.getId())
+        mockMvc.perform(delete("/{id}", reviewList.get(3).getId())
                     .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void givenNoReview_whenDeleteReview_thenStatusNotFound() throws Exception {
+        mockMvc.perform(delete("/{id}", "ABC123testId")
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 }
