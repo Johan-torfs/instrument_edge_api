c0d374ccae6e84bd041037a96194115ed45a486b
JohanTorfs <r0831282@student.thomasmore.be>
2022-11-11 19:25:50 +0100

    no smelly code

--- counter_api/src/main/java/com/example/counter_api/controller/CounterController.java
+++ counter_api/src/main/java/com/example/counter_api/controller/CounterController.java
@@ -1,17 +1,11 @@
 package com.example.counter_api.controller;
 
-import javax.annotation.PostConstruct;
-
 import com.example.counter_api.model.Counter;
 import com.example.counter_api.repository.CounterRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.tinylog.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
